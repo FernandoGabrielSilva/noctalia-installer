26737adba0699768ea510fca091ae3e765f15b8f
Fernando Gabriel Silva <107934284+FernandoGabrielSilva@users.noreply.github.com>
2026-03-17 16:05:21 -0300

    Delete lib directory

--- lib/install.sh
+++
@@ -1,9 +0,0 @@
-install_deps() {
-PKG=$1
-
-if [ "$PKG" = "pacman" ]; then
-sudo pacman -S --noconfirm git curl brightnessctl cava wl-clipboard
-else
-sudo apt install -y git curl brightnessctl cava wl-clipboard
-fi
-}
